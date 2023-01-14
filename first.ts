d64ecabc0a369dcba0cc2aa63a0845f0ea6b0fd8
LuisKlopp <fbgus333@gmail.com>
2023-01-15 03:11:19 +0900

    타입스크립트 문법

---
+++ first.ts
@@ -0,0 +1,22 @@
+const a: number = 5;
+const b: string = '5';
+const c: boolean = true;
+const d: undefined = undefined;
+const e: null = null;
+const f: 5 = 5;
+
+// function add(x: number, y: number): number {
+//   return x + y;
+// }
+// type Add = (x: number, y: number) => number;
+// const add: Add = (x, y) => x + y;
+
+// interface Add {
+//   (x: number, y: number): number;
+// }
+
+// const add: Add = (x, y) => x + y;
+const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
+const arr: string[] = ['123', '456'];
+const arr2: Array<number> = [123, 456];
+const arr3: [number, number, string] = [123, 456, 'hello'];
